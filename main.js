6f6e99d894f1c35d122d3605536954ec68daacb0
anitej6 <103579202+anitej6@users.noreply.github.com>
2022-06-22 17:26:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+<html>
+    <head>
+        <title> <h4>Project on console</h4></title>
+    </head>
+    <body>
+        <p>Write about how to open console screen.</p>
+        <p>write about console and how to open it.</p>
+        
+        Var first_name="anitej"
+        console.log(first_name)
+        var last_name="sharma"
+        console.log(last_name)
+        
+        
+        
+    </body>
+</html>
